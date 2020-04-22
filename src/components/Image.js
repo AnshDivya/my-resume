53ccddfbc7b35ee320f8c546fe4d82feb4d66801
AnshDivya <divyansh15.dt@gmail.com>
2020-04-22 20:57:08 +0530

    some minor changes

--- src/components/Image.js
+++ src/components/Image.js
@@ -1,19 +1,34 @@
 import React from 'react';
 import '../myStyles/Image.css'
 import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
-function Image(props) {
-    return (
-        <ReactCSSTransitionGroup
-            transitionName="image_leftToRight"
-            transitionAppear={true}
-            transitionAppearTimeout={1500}
-            transitionEnter={false}
-            transitionLeave={false}
-        >
-            <img alt = 'coding' className = {props.styleClass}  src = {props.src} />             
-        </ReactCSSTransitionGroup>
+class Image extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <ReactCSSTransitionGroup
+                transitionName="image_leftToRight"
+                transitionAppear={true}
+                transitionAppearTimeout={1500}
+                transitionEnter={false}
+                transitionLeave={false}
+            >
+                <img alt = 'coding' className = {this.props.styleClass}  src = {this.props.src} />             
+            </ReactCSSTransitionGroup>
+        );
+
         
-    );
+    }
+
+    componentDidMount() {
+        if(this.props.styleClass === 'profilePic') {
+            console.log('HEY THE IMAGE HAS BEEN LOADED');
+            
+        }
+    }
+    
 }
 
 export default Image;
