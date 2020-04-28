4557cce19f7a60aa0f7516b49ff3e46b280d6936
AnshDivya <divyansh15.dt@gmail.com>
2020-04-28 16:33:04 +0530

    add button

--- src/components/header.js
+++ src/components/header.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import '../myStyles/header.css';
+import MenuIcon from '@material-ui/icons/Menu';
 
 import NavigatorButton from './navigator-button';
 function Header(props) {
@@ -7,7 +8,17 @@ function Header(props) {
         <div className = 'header'>
             <div className = 'header-name-container'>
                 <h1>Divyansh Thakur</h1>
+                <MenuIcon style = {
+                    {
+                        color: '#182153',
+                        height: '70px',
+                        fontSize: '40px',
+                        float: 'right',
+                        marginRight: '10px'
+                    }
+                } />
             </div>
+            
             <div className = 'header-navigation-button-container'>
                 <NavigatorButton itemName = 'EXPERIENCE' {...props}/>
                 <NavigatorButton itemName = 'EDUCATION' {...props}/>
